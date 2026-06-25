bb4d6342fb95e827e6e64d89d9de40254c6d9070
Dirgaaa <dirgayuditama6@gmail.com>
2026-06-25 22:17:33 +0700

    fix: fix navbar behavior

--- src/shared/components/Navbar.tsx
+++ src/shared/components/Navbar.tsx
@@ -23,73 +23,74 @@ export function Navbar() {
   return (
     <nav
       ref={navRef}
-      style={{ opacity: 0 }}
-      className="absolute inset-x-0 w-full z-30 flex mx-auto items-center justify-between max-w-360 gap-6 px-5 py-5 md:px-10"
+      className="fixed w-full z-9999 flex justi items-center backdrop-blur-lg"
     >
-      {/* Logo */}
-      <div className="flex shrink-0 items-center">
-        <Image
-          src="/batik-sengguruh.svg"
-          alt="Batik Sengguruh"
-          width={152}
-          height={40}
-          className="h-10 w-auto object-contain"
-          priority
-        />
-      </div>
+      <div className="flex w-full max-w-360 gap-6 px-5 py-4 md:py-5 md:px-10 mx-auto justify-between">
+        {/* Logo */}
+        <div className="flex shrink-0 items-center">
+          <Image
+            src="/batik-sengguruh.svg"
+            alt="Batik Sengguruh"
+            width={152}
+            height={40}
+            className="h-10 w-auto object-contain"
+            priority
+          />
+        </div>
 
-      <div className="hidden md:flex flex-1 items-center justify-center gap-1 rounded-full bg-[#966b4c] px-6 py-2.5 shadow-lg max-w-135">
-        {NAV_LINKS.map((link) => (
-          <a
-            key={link}
-            href="#n"
-            className="rounded-full px-3.5 py-1 text-xs font-semibold uppercase tracking-wider text-white/90 transition-colors hover:text-white"
-          >
-            {link}
-          </a>
-        ))}
-      </div>
-
-      <a
-        href="#shop"
-        className="hidden md:block shrink-0 rounded-full bg-[#966b4c] px-6 py-3 text-sm text-white shadow-md transition-all hover:bg-[#966b4c]/80"
-      >
-        Shop Now
-      </a>
-
-      <button
-        type="button"
-        onClick={() => setMenuOpen(!menuOpen)}
-        className={`hamburger hamburger--collapse md:hidden! scale-60 p-0! ${menuOpen ? "is-active" : ""}`}
-        aria-label="Toggle menu"
-      >
-        <span className="hamburger-box">
-          <span className="hamburger-inner bg-white! before:bg-white! after:bg-white!" />
-        </span>
-      </button>
-
-      {/* Mobile menu dropdown */}
-      {menuOpen && (
-        <div className="absolute top-full left-5 right-5 z-40 flex md:hidden flex-col gap-3 bg-[#966b4c]/95 backdrop-blur-md p-6 shadow-2xl rounded-2xl border border-white/10 animate-in fade-in slide-in-from-top-4 duration-300">
+        <div className="hidden md:flex flex-1 items-center justify-center gap-1 rounded-full bg-[#966b4c] px-6 py-2.5 shadow-lg max-w-135">
           {NAV_LINKS.map((link) => (
             <a
               key={link}
               href="#n"
-              onClick={() => setMenuOpen(false)}
-              className="py-3 px-4 rounded-xl text-center text-sm font-semibold uppercase tracking-wider text-white/90 hover:bg-white/10 hover:text-white transition-all duration-200"
+              className="rounded-full px-3.5 py-1 text-xs font-semibold uppercase tracking-wider text-white/90 transition-colors hover:text-white"
             >
               {link}
             </a>
           ))}
-          <a
-            href="#shop"
-            onClick={() => setMenuOpen(false)}
-            className="mt-2 text-center rounded-xl bg-white text-[#966b4c] py-3 text-sm font-extrabold shadow-lg transition-all duration-200 hover:scale-[1.01] active:scale-[0.99]"
-          >
-            Shop Now
-          </a>
         </div>
-      )}
+
+        <a
+          href="#shop"
+          className="hidden md:block shrink-0 rounded-full bg-[#966b4c] px-6 py-3 text-sm text-white shadow-md transition-all hover:bg-[#966b4c]/80"
+        >
+          Shop Now
+        </a>
+
+        <button
+          type="button"
+          onClick={() => setMenuOpen(!menuOpen)}
+          className={`hamburger hamburger--collapse md:hidden! scale-60 p-0! ${menuOpen ? "is-active" : ""}`}
+          aria-label="Toggle menu"
+        >
+          <span className="hamburger-box">
+            <span className="hamburger-inner bg-white! before:bg-white! after:bg-white!" />
+          </span>
+        </button>
+
+        {/* Mobile menu dropdown */}
+        {menuOpen && (
+          <div className="absolute top-full left-5 right-5 z-40 flex md:hidden flex-col gap-3 bg-[#966b4c]/95 backdrop-blur-md p-6 shadow-2xl rounded-2xl border border-white/10 animate-in fade-in slide-in-from-top-4 duration-300">
+            {NAV_LINKS.map((link) => (
+              <a
+                key={link}
+                href="#n"
+                onClick={() => setMenuOpen(false)}
+                className="py-3 px-4 rounded-xl text-center text-sm font-semibold uppercase tracking-wider text-white/90 hover:bg-white/10 hover:text-white transition-all duration-200"
+              >
+                {link}
+              </a>
+            ))}
+            <a
+              href="#shop"
+              onClick={() => setMenuOpen(false)}
+              className="mt-2 text-center rounded-xl bg-white text-[#966b4c] py-3 text-sm font-extrabold shadow-lg transition-all duration-200 hover:scale-[1.01] active:scale-[0.99]"
+            >
+              Shop Now
+            </a>
+          </div>
+        )}
+      </div>
     </nav>
   );
 }
